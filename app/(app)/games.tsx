eb498278f896e2a491083256db1626b882a53abe
Moh88CS <97626793+Moh88CS@users.noreply.github.com>
2023-11-02 21:53:23 -0400

    Fetch data from the db (#31)
    -printed data from collections to console
    -added temp games list w/load

--- app/(app)/games.tsx
+++ app/(app)/games.tsx
@@ -1,16 +1,50 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Text, StyleSheet, ActivityIndicator } from 'react-native';
+import useGames from '../../components/gamesFetcher';
+import useQuestions from '../../components/questionsFetcher';
+import useTeams from '../../components/teamsFetcher';
+import useUsers from '../../components/usersFetcher';
 
-// create a component
-const GamesPage = () => {
-    return (
-        <View style={styles.container}>
-            <Text>Games Page</Text>
-        </View>
-    );
+// recall needed data from the db from the fetch components
+const GamesPage: React.FC = () => {
+  const { games, loading: gamesLoading } = useGames(); // the loading state with games only because we will only print games
+  const { questions } = useQuestions();
+  const { teams } = useTeams();
+  const { users } = useUsers();
+
+  // display the data in the console for testing
+  useEffect(() => {
+    if (!gamesLoading) { 
+      console.log("Games:", games)
+      console.log("Questions:", questions);
+      console.log("Teams:", teams);
+      console.log("Users:", users);
+    }
+  }, [gamesLoading, questions, teams, users]);
+
+  // print games data for testing
+  return (
+    <View style={styles.container}>
+      {gamesLoading ? (
+        <ActivityIndicator size="large" color="#0000ff" /> // temporary loading circle untill data is displayed
+      ) : (
+        <>
+          <Text>Games Page</Text>
+          <Text>Games:</Text>
+          {games.map((game, index) => (
+            <View key={index}>
+              <Text>Game {index + 1}</Text>
+              <Text>Question Time: {game.questionTime}</Text>
+              <Text>Start Time: {game.startTime.toDate().toLocaleString()}</Text>
+              {/* References are omitted and was unable to print */}
+            </View>
+          ))}
+        </>
+      )}
+    </View>
+  );
 };
 
-// define your styles
 const styles = StyleSheet.create({
     container: {
         flex: 1,
@@ -19,5 +53,4 @@ const styles = StyleSheet.create({
     },
 });
 
-//make this component available to the app
 export default GamesPage;

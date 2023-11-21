511031fcc70f4f9e19a9c602364593d9a2ba261c
Moh88CS <mohamed.abdulal88@gmail.com>
2023-11-20 21:20:12 -0500

    Fix eslint errors
    -installed required dependencies
    -updated .eslintrc file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,18 @@
 module.exports = {
     root: true,
-    extends: ['@react-native-community', 'airbnb-typescript', 'prettier', 'prettier/@typescript-eslint', 'prettier/react'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint', 'react', 'react-hooks', 'prettier'],
+    rules: {
+        '@typescript-eslint/no-shadow': 'off',
+        '@typescript-eslint/no-var-requires': 'off'
+    },
+    extends: [
+        '@react-native-community',
+        'eslint:recommended',
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended', 
+        'plugin:react-hooks/recommended',
+        'prettier'
+    ]
 };

50b5e117f2576c0c46c321ab3deae47049613b18
brennamehl <brennam@email.unc.edu>
2023-12-07 10:38:54 -0500

    Set up routing to game page

--- app/(app)/home.tsx
+++ app/(app)/home.tsx
@@ -1,6 +1,6 @@
-import { Link, useRouter } from 'expo-router';
+import { Href, Link, useRouter } from 'expo-router';
 import React from 'react';
-import { View, Text, StyleSheet, Pressable, TextInput } from 'react-native';
+import { View, Text, StyleSheet, Pressable, TextInput, ActivityIndicator } from 'react-native';
 import { FIREBASE_AUTH } from '../../FirebaseConfig';
 import GameCard from '../../components/GameCard';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
@@ -14,27 +14,58 @@ const HomePage = () => {
     const { games, loading: gamesLoading } = useGames();
     const [text, onChangeText] = React.useState('');
 
+    // shows loading screen when waiting to fetch games
+    // if(gamesLoading){
+    //     return (
+    //         <ActivityIndicator
+    //             size="large"
+    //             color="#0000ff"
+    //         />)
+    // }
+
     return (
         <View style={[styles.container, styles.dark]}>
             <View style={styles.input}>
-                <MaterialCommunityIcons name="key-variant" size={20} color="white" />
-                <TextInput style={styles.textInput} value={text} placeholder={'Enter a code to join the game'}
-                    placeholderTextColor='white' onChangeText={onChangeText} />
+                <MaterialCommunityIcons
+                    name="key-variant"
+                    size={20}
+                    color="white"
+                />
+                <TextInput
+                    style={styles.textInput}
+                    value={text}
+                    placeholder={'Enter a code to join the game'}
+                    placeholderTextColor="white"
+                    onChangeText={onChangeText}
+                />
             </View>
-            <Pressable onPress={()=> console.log('Button Pressed')}
+            <Pressable
+                onPress={() => console.log('Button Pressed')}
                 style={({ pressed }) => [
                     styles.button,
-                    pressed && styles.buttonPressed,
-                  ]}
+                    pressed && styles.buttonPressed
+                ]}
             >
                 <Text style={styles.buttonText}>Join</Text>
             </Pressable>
-            <View style={{borderBottomColor: 'white', borderBottomWidth: 2, marginBottom: 5}}>
+            <View
+                style={{
+                    borderBottomColor: 'white',
+                    borderBottomWidth: 2,
+                    marginBottom: 5
+                }}
+            >
                 <Text style={styles.upcoming}>Upcoming Games</Text>
             </View>
             <View style={styles.games}>
                 {games.map((game, index) => (
-                    <GameCard key={index} game={game} />
+                    <GameCard
+                        key={index}
+                        game={game}
+                        onPress={() => {
+                            router.push(`/game/${game.gameID}` as any);
+                        }}
+                    />
                 ))}
             </View>
         </View>
@@ -48,24 +79,24 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         flexDirection: 'column',
-        padding: 20,
+        padding: 20
     },
     dark: {
-        backgroundColor: '#253031',
+        backgroundColor: '#253031'
     },
     input: {
         borderBottomColor: 'white',
         borderBottomWidth: 2,
         padding: 10,
         width: 360,
-        flexDirection: 'row',
+        flexDirection: 'row'
     },
     textInput: {
         fontWeight: '300',
         color: 'white',
         width: 340,
         paddingLeft: 20,
-        fontSize: 18,
+        fontSize: 18
     },
     button: {
         backgroundColor: '#DDE819',
@@ -73,7 +104,7 @@ const styles = StyleSheet.create({
         shadowColor: '#000',
         shadowOffset: {
             width: 0,
-            height: 4,
+            height: 4
         },
         shadowOpacity: 0.5,
         shadowRadius: 6,
@@ -82,7 +113,7 @@ const styles = StyleSheet.create({
         height: 50,
         alignContent: 'center',
         justifyContent: 'center',
-        margin: 20,
+        margin: 20
     },
     buttonText: {
         fontSize: 24,
@@ -90,11 +121,11 @@ const styles = StyleSheet.create({
         alignContent: 'center',
         textAlign: 'center',
         padding: 5,
-        fontWeight: '500',
+        fontWeight: '500'
     },
     buttonPressed: {
         opacity: 0.8,
-        transform: [{ scale: 0.96 }],
+        transform: [{ scale: 0.96 }]
     },
     upcoming: {
         fontSize: 30,
@@ -104,13 +135,12 @@ const styles = StyleSheet.create({
         padding: 5,
         borderBottomColor: 'white',
         borderBottomWidth: 2,
-        width: 360,
+        width: 360
     },
     games: {
         justifyContent: 'flex-start',
-        flex: 1,
-
-    },
+        flex: 1
+    }
 });
 
 //make this component available to the app
---
+++ screens/LobbyScreen.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { View, Text, StyleSheet, Modal, Button } from 'react-native';
+import { Game } from '../types/Game';
+import { ViewProps } from 'react-native/Libraries/Components/View/ViewPropTypes';
+import { Team } from '../types/Team';
+
+interface LobbyProps extends ViewProps {
+    game: Game;
+}
+
+const LobbyScreen: React.FC<LobbyProps> = ({ game }) => {
+    //const [teamSelected, setTeamSelected] = React.useState(false);
+    const [team, setTeam] = React.useState('');
+    return (
+        <View style={styles.container}>
+            <Modal
+                // I have a feeling that this is not the right way to do
+                // this may need a boolean variable
+                visible={team !== ''}
+            >
+                <Text>Choose a team</Text>
+                {/* replace buttons with team logos */}
+                <Button
+                    title={game.team1.name}
+                    onPress={() => setTeam(game.team1.name)}
+                ></Button>
+                <Button
+                    title={game.team1.name}
+                    onPress={() => setTeam(game.team1.name)}
+                ></Button>
+            </Modal>
+            <Text> You're playing for {team}.</Text>
+            <Text>Waiting for others to join...</Text>
+        </View>
+    );
+};
+
+// define your styles
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
+
+//make this component available to the app
+export default LobbyScreen;

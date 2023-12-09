5ab40feb3d7a8fd05b5aec480b4321a49f6d074f
brennamehl <brennam@email.unc.edu>
2023-12-09 12:25:02 -0500

    Add choose team popup modal

--- screens/GameModal.tsx
+++ screens/GameModal.tsx
@@ -1,71 +1,87 @@
-import { Button, Modal, Text, View } from "react-native";
-import { Game } from "../types/Game";
-import { useEffect, useState } from "react";
-import LobbyScreen from "./LobbyScreen";
-import subscribeToGameStateChanges from "../services/subscribeToGameState";
-import useGame from "../services/useGame";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { StyleSheet } from "react-native";
+import { Button, Modal, Text, View } from 'react-native';
+import { Game } from '../types/Game';
+import { useEffect, useState } from 'react';
+import LobbyScreen from './LobbyScreen';
+import subscribeToGameStateChanges from '../services/subscribeToGameState';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { StyleSheet } from 'react-native';
+import { Team } from '../types/Team';
 
 interface GameModalProps {
     visible: boolean;
     onClose: () => void;
     game: Game;
-  }
+}
 
-const GameModal: React.FC<GameModalProps> = ({visible, onClose, game}) => {
-    // const {game, loading: gameLoading} = useGame(gameID);
+const GameModal: React.FC<GameModalProps> = ({ visible, onClose, game }) => {
+    const [team, setTeam] = useState<Team>();
     const [gameState, setGameState] = useState<string>(game.gameState);
-    console.log('Game state is: ' + gameState)
-
+    
     useEffect(() => {
-      if (game.gameID) {
-        const unsubscribeGameState = subscribeToGameStateChanges(game.gameID, (newGameState) => {
-          setGameState(newGameState);
-        });
-  
-        return () => {
-          // Clean up the subscription when the modal is unmounted
-          unsubscribeGameState();
-        };
-      }
+        if (game.gameID) {
+            const unsubscribeGameState = subscribeToGameStateChanges(
+                game.gameID,
+                (newGameState) => {
+                    setGameState(newGameState);
+                }
+            );
+
+            return () => {
+                // Clean up the subscription when the modal is unmounted
+                unsubscribeGameState();
+            };
+        }
     }, [game.gameID]);
-  
-    if (game === null) {
-      onClose();
-      return null; // or return a loading state
+
+    if (game === null || gameState === undefined) {
+        onClose();
+        return null;
     }
-  
+
     return (
-        <Modal visible={visible} onRequestClose={onClose} animationType="slide" >  
+        <Modal visible={visible} onRequestClose={onClose} animationType="slide">
             <SafeAreaView style={styles.modalBackground}>
-            {gameState===undefined && 
-                    (<Text>Undefined</Text>)}
-                {gameState==='inactive' && 
-                    (<View>
+                {/* force users to select a team if they haven't already */}
+                <Modal visible={team === undefined}>
+                    <SafeAreaView style={styles.modalBackground}>
+                        <Text>Choose a team</Text>
+                        {/* replace buttons with team logos */}
+                        <Button
+                            title={game.team1.name}
+                            onPress={() => setTeam(game.team1)}
+                        ></Button>
+                        <Button
+                            title={game.team2.name}
+                            onPress={() => setTeam(game.team2)}
+                        ></Button>
+                    </SafeAreaView>
+                </Modal>
+                {gameState === undefined && <Text>Undefined</Text>}
+                {gameState === 'inactive' && (
+                    <View>
                         <Text>Inactive Page</Text>
-                    </View>)}
-                {gameState==='lobby' && 
-                    (<LobbyScreen game={game}></LobbyScreen> )}
-                {gameState==='question' && 
-                    (<Text>Question Page</Text>)}
-                {gameState==='leaderboard' && 
-                    (<Text>Leaderboard Page</Text>)}
-                {gameState==='finalLeaderboard' &&
-                    (<Text>Final Leaderboard Page</Text>)}
-                <Button title='Leave Game' color={'red'} onPress={onClose}/>
-            </SafeAreaView> 
-        </Modal>)
-            
+                    </View>
+                )}
+                {gameState === 'lobby' && (
+                    <LobbyScreen game={game} team={team}></LobbyScreen>
+                )}
+                {gameState === 'question' && <Text>Question Page</Text>}
+                {gameState === 'leaderboard' && <Text>Leaderboard Page</Text>}
+                {gameState === 'finalLeaderboard' && (
+                    <Text>Final Leaderboard Page</Text>
+                )}
+                <Button title="Leave Game" color={'red'} onPress={onClose} />
+            </SafeAreaView>
+        </Modal>
+    );
 };
 
 const styles = StyleSheet.create({
     modalBackground: {
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
-      },
-    });
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+});
 
-export default GameModal; 
-
+export default GameModal;
--- screens/LobbyScreen.tsx
+++ screens/LobbyScreen.tsx
@@ -1,37 +1,28 @@
 import React from 'react';
-import { View, Text, StyleSheet, Modal, Button } from 'react-native';
+import { Text, StyleSheet, SafeAreaView } from 'react-native';
 import { Game } from '../types/Game';
 import { ViewProps } from 'react-native/Libraries/Components/View/ViewPropTypes';
 import { Team } from '../types/Team';
 
 interface LobbyProps extends ViewProps {
     game: Game;
+    team?: Team;
 }
 
-const LobbyScreen: React.FC<LobbyProps> = ({ game }) => {
+const LobbyScreen: React.FC<LobbyProps> = ({ game, team }) => {
     //const [teamSelected, setTeamSelected] = React.useState(false);
-    const [team, setTeam] = React.useState('');
-    return ( <Text>Lobby View</Text>
-        // <View style={styles.container}>
-        //     <Modal
-        //         // I have a feeling that this is not the right way to do
-        //         // this may need a boolean variable
-        //         visible={team !== ''}
-        //     >
-        //         <Text>Choose a team</Text>
-        //         {/* replace buttons with team logos */}
-        //         <Button
-        //             title={game.team1.name}
-        //             onPress={() => setTeam(game.team1.name)}
-        //         ></Button>
-        //         <Button
-        //             title={game.team1.name}
-        //             onPress={() => setTeam(game.team1.name)}
-        //         ></Button>
-        //     </Modal>
-        //     <Text> You're playing for {team}.</Text>
-        //     <Text>Waiting for others to join...</Text>
-        // </View>
+    const opponent =
+        team?.teamID === game.team1.teamID ? game.team2.name : game.team1.name;
+    return (
+        <SafeAreaView style={styles.container}>
+            <Text>
+                Welcome to FanzPlay! You're in.
+            </Text>
+            <Text>
+                Ready to beat {opponent}?
+            </Text>
+            <Text>Waiting for others to join...</Text>
+        </SafeAreaView>
     );
 };
 
@@ -39,7 +30,7 @@ const LobbyScreen: React.FC<LobbyProps> = ({ game }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
         alignItems: 'center'
     }
 });

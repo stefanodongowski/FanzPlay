e6963dc881e065235713249b34b0b6bb8bce853f
Maya Theresa Gopal <mayather@email.unc.edu>
2023-12-10 01:26:48 -0500

    Style leaderboard page

--- screens/LeaderboardScreen.tsx
+++ screens/LeaderboardScreen.tsx
@@ -1,4 +1,4 @@
-import { View, ViewProps, Text } from "react-native";
+import { View, ViewProps, Text, StyleSheet } from "react-native";
 import { Game } from "../types/Game";
 import { Team } from "../types/Team";
 
@@ -16,18 +16,88 @@ const LeaderboardScreen: React.FC<LeaderboardScreenProps> = ({
 }) => {
 
     return (
-        <View>
-            <Text>Leaderboard Page</Text>
-            <Text>
-                {game.team1.name} Score:{' '}
+        <View style={[styles.container, styles.dark]}>
+            <View
+                style={{
+                    borderBottomColor: 'white',
+                    borderBottomWidth: 2,
+                    marginBottom: 5
+                }}
+            >
+                <Text style={styles.leaderboardText}>Leaderboard</Text>
+            </View>
+            <Text style={styles.answerText}>
+                The correct answer to the previous question was... 
+            </Text>
+            <Text style={styles.answer}>
+                {game.questions[currentQuestion].answers[game.questions[currentQuestion].correctAnswer]}!
+            </Text>
+            <Text style={styles.currentScoreText}>The current standings are...</Text>
+            <Text style={styles.scoreText}>
+                {game.team1.name}:{' '}
                 {(game.team1score / game.team1responses) * 100}
             </Text>
-            <Text>
-                {game.team2.name} Score:{' '}
+            <Text style={styles.scoreText}>
+                {game.team2.name}:{' '}
                 {(game.team2score / game.team2responses) * 100}
             </Text>
         </View>
     );
-}
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        padding: 20,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    dark: {
+        backgroundColor: '#253031'
+    },
+    leaderboardText: {
+        fontSize: 40,
+        fontWeight: '600',
+        textAlign: 'center',
+        color: 'white',
+        padding: 5,
+        borderBottomColor: 'white',
+        borderBottomWidth: 2,
+        width: 360
+    },
+    answerText: {
+        color: 'white',
+        alignContent: 'center',
+        textAlign: 'center',
+        padding: 5,
+        fontSize: 20,
+        fontWeight: '400'
+    },
+    answer: {
+        color: 'white',
+        alignContent: 'center',
+        textAlign: 'center',
+        padding: 5,
+        fontWeight: '400',
+        marginBottom: 30,
+        fontSize: 20
+    },
+    currentScoreText: {
+        color: 'white',
+        textAlign: 'center',
+        padding: 10,
+        width: 360,
+        fontSize: 20,
+        marginBottom: 20
+    },
+    scoreText: {
+        color: 'white',
+        alignContent: 'center',
+        textAlign: 'center',
+        padding: 5,
+        fontSize: 20,
+        fontWeight: '400'
+    },
+});
 
 export default LeaderboardScreen;

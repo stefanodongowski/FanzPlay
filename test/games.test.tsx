827558f61a48ff334c08e21447e876d6e1a90ec2
brennamehl <brennam@email.unc.edu>
2023-12-12 15:20:43 -0500

    Complete games and users tests

--- test/games.test.tsx
+++ test/games.test.tsx
@@ -1,35 +1,39 @@
 import * as firebase from '@firebase/testing';
 import { userAuth, getFirestore, getAdminFirestore, cleanupFirestore} from './testUtils';
-import { DocumentReference, Timestamp, doc, updateDoc } from 'firebase/firestore';
-import { Game } from '../types/Game';
 
 beforeEach(cleanupFirestore);
 
 // Tests read, create, update, and delete functions for our Games Firestore collection
 describe('Games Firestore collection',  () => {
-    it("Allows all users to read documents", async () => {
-        const db = getFirestore();
+    it("Allows authenticated users to read documents", async () => {
+        const db = getFirestore(userAuth);
         const testDoc = db.collection('games').doc('testDoc1');
         await firebase.assertSucceeds(testDoc.get());
     })
 
+    it('Allows unauthenticated users to read documents', async () => {
+        const db = getFirestore();
+        const testDoc = db.collection('games').doc('testDoc2');
+        await firebase.assertSucceeds(testDoc.get());
+    });
+
     it('Allows authenticated users to write to the collection', async () => {
         const db = getFirestore(userAuth);
-        const testDoc = db.collection('games').doc('testDoc2');
-        await firebase.assertSucceeds(testDoc.set({ username: 'user_1' }));
+        const testDoc = db.collection('games').doc('testDoc3');
+        await firebase.assertSucceeds(testDoc.set({ gameState: 'inactive' }));
     });
 
     it('Prevents unauthenticated users to write to the collection', async () => {
         const db = getFirestore();
-        const testDoc = db.collection('games').doc('testDoc2');
-        await firebase.assertFails(testDoc.set({ username: 'test' }));
+        const testDoc = db.collection('games').doc('testDoc4');
+        await firebase.assertFails(testDoc.set({ gameState: 'inactive' }));
     });
 
     it('Allows authenticated users to update documents', async () => {
         const adminDb = getAdminFirestore();
         const gameId = "test_game_id";
         const setupDoc = adminDb.collection("games").doc(gameId);
-        await setupDoc.set({gameId: gameId, gameState: 'undefined'})
+        await setupDoc.set({gameID: gameId, gameState: 'inactive'})
 
         const db = getFirestore(userAuth);
         const testDoc = db.collection('games').doc(gameId);
@@ -42,12 +46,12 @@ describe('Games Firestore collection',  () => {
         const adminDb = getAdminFirestore();
         const gameId = 'test_game_id';
         const setupDoc = adminDb.collection('games').doc(gameId);
-        await setupDoc.set({ gameId: gameId, gameState: 'undefined' });
+        await setupDoc.set({ gameID: gameId, gameState: 'inactive' });
 
         const db = getFirestore();
         const testDoc = db.collection('games').doc(gameId); 
         await firebase.assertFails(
-            testDoc.update({ updatedField: 'lobby' })
+            testDoc.update({ gameState: 'lobby' })
         );
     });
 
@@ -55,7 +59,7 @@ describe('Games Firestore collection',  () => {
         const adminDb = getAdminFirestore();
         const gameId = 'test_game_id';
         const setupDoc = adminDb.collection('games').doc(gameId);
-        await setupDoc.set({ gameId: gameId, gameState: 'undefined' });
+        await setupDoc.set({ gameId: gameId, gameState: 'inactive' });
 
         const db = getFirestore(userAuth);
         const testDoc = db.collection('games').doc(gameId); 
@@ -66,7 +70,7 @@ describe('Games Firestore collection',  () => {
         const adminDb = getAdminFirestore();
         const gameId = 'test_game_id';
         const setupDoc = adminDb.collection('games').doc(gameId);
-        await setupDoc.set({ gameId: gameId, gameState: 'undefined' });
+        await setupDoc.set({ gameId: gameId, gameState: 'inactive' });
 
         const db = getFirestore();
         const testDoc = db.collection('games').doc(gameId); 
--- test/users.test.tsx
+++ test/users.test.tsx
@@ -1,26 +1,82 @@
 import * as firebase from '@firebase/testing';
-import { userAuth, getFirestore, cleanupFirestore } from './testUtils';
+import {
+    userAuth,
+    getFirestore,
+    getAdminFirestore,
+    cleanupFirestore
+} from './testUtils';
+
+beforeEach(cleanupFirestore);
 
 // Tests read, create, update, and delete functions for our Users Firestore collection
-describe('Users Firestore collection',  () => {
-    it("Allows all users to read documents", async () => {
-        const db = getFirestore();
+describe('Users Firestore collection', () => {
+    it('Allows authenticated users to read user documents', async () => {
+        const db = getFirestore(userAuth);
         const testDoc = db.collection('users').doc('testDoc1');
         await firebase.assertSucceeds(testDoc.get());
-    })
+    });
 
-    it('Allows authenticated users to write to the collection', async () => {
+    it('Allows unauthenticated users to read user documents', async () => {
+        const db = getFirestore();
+        const testDoc = db.collection('users').doc('users');
+        await firebase.assertSucceeds(testDoc.get());
+    });
+
+    it('Allows authenticated users to write to the users collection', async () => {
         const db = getFirestore(userAuth);
         const testDoc = db.collection('users').doc('testDoc2');
         await firebase.assertSucceeds(testDoc.set({ username: 'user_1' }));
     });
 
-    it('Does not allow unauthenticated users to write to the collection', async () => {
+    it('Prevents unauthenticated users to write to the users collection', async () => {
         const db = getFirestore();
-        const testDoc = db.collection('users').doc('testDoc3');
+        const testDoc = db.collection('users').doc('testDoc2');
         await firebase.assertFails(testDoc.set({ username: 'test' }));
     });
 
-})
+    it('Allows authenticated users to update user documents', async () => {
+        const adminDb = getAdminFirestore();
+        const userId = 'test_user_id1';
+        const setupDoc = adminDb.collection('users').doc(userId);
+        await setupDoc.set({userID: userId, username: 'username1' });
+
+        const db = getFirestore(userAuth);
+        const testDoc = db.collection('users').doc(userId);
+        await firebase.assertSucceeds(testDoc.update({ username: 'newUsername1' }));
+    });
+
+    it('Prevents unauthenticated users from updating user documents', async () => {
+        const adminDb = getAdminFirestore();
+        const userId = 'test_user_id2';
+        const setupDoc = adminDb.collection('users').doc(userId);
+        await setupDoc.set({ userID: userId, username: 'username2' });
+
+        const db = getFirestore();
+        const testDoc = db.collection('users').doc(userId);
+        await firebase.assertFails(testDoc.update({ username: 'newUsername2' }));
+    });
+
+    it('Allows authenticated users to delete user documents', async () => {
+        const adminDb = getAdminFirestore();
+        const userId = 'test_user_id3';
+        const setupDoc = adminDb.collection('users').doc(userId);
+        await setupDoc.set({ userID: userId, username: 'username3' });
+
+        const db = getFirestore(userAuth);
+        const testDoc = db.collection('users').doc(userId);
+        await firebase.assertSucceeds(testDoc.delete());
+    });
+
+    it('Prevents unauthenticated users from deleting user documents', async () => {
+        const adminDb = getAdminFirestore();
+        const userId = 'test_user_id4';
+        const setupDoc = adminDb.collection('users').doc(userId);
+        await setupDoc.set({ userID: userId, username: 'username4' });
+
+        const db = getFirestore();
+        const testDoc = db.collection('users').doc(userId);
+        await firebase.assertFails(testDoc.delete());
+    });
+});
 
 afterAll(cleanupFirestore);
